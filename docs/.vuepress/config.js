8032a67b263d843c297c26e8fc81e6f49df642dd
leejooy96 <leejooy96@gmail.com>
2022-08-10 17:15:35 +0900

    package: add vuepress sidebar and configuration file

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+const getConfig = require("vuepress-bar");
+
+const { nav, sidebar } = getConfig({
+    // See https://github.com/ozum/vuepress-bar
+})
+
+module.exports = {
+    title: `TIL`,
+    description: `Today I Learned`,
+    dest: 'build',
+    /*head: [
+        ['link', {
+            rel: 'icon',
+            href: '/logo.png'
+        }]
+    ],*/
+    themeConfig: {
+        sidebar,
+        nav: [
+            {
+                text: 'GitHub',
+                link: 'https://github.com/jooy2/TIL'
+            }, {
+                text: 'Blog',
+                link: 'https://jootc.com'
+            },
+            ...nav
+        ]
+    },
+}
